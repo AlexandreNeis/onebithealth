3a571374dc5295a07557137e1c42e9c2be5f7e3a
Alexandre Neis <alexandre.neis@hotmail.com>
2023-04-06 09:49:54 -0300

    alter name title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ import styles from './style'
 export default function Title(){
     return(
         <View style={styles.boxTitle}>
-            <Text style={styles.textTitle}>Calcular IMC</Text>
+            <Text style={styles.textTitle}>ONE BIT HEALTH</Text>
         </View>
     )
 }
